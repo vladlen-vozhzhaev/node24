90905099a2b117cf2a46a95312ceb1b31eae96c8
Vladlen <vladlen@vozhzhaev.ru>
2022-08-26 21:26:49 +0300

    Сделали авторизацию

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// решить проблему с регистром логина
 const express = require('express')
 const app = express()
 const multer = require('multer');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const cookieParser = require('cookie-parser');
 const urlencodedParser = express.urlencoded();
 const { engine } = require ('express-handlebars');
 const connection = mysql.createConnection({
@@ -10,15 +14,22 @@ const connection = mysql.createConnection({
     database: 'node24',
     password: ''
 });
+function getIntRandom(min, max){
+    Math.floor(Math.random()*(max-min)+min)
+}
 app.engine('handlebars', engine());
 app.set('views', './views');
 app.set('view engine', 'handlebars');
+app.use(cookieParser('secret'))
 
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req,res)=>{
     res.statusCode = 200;
-    res.render('home', {title: "Главная страница", content: "Hello world"})
+    res.render('home',
+        {title: "Главная страница",
+            content: "Hello world",
+            auth: req.cookies.token});
 })
 app.get('/reg', (req, res)=>{
    res.render('reg', {title: "Регистрация"});
@@ -29,15 +40,33 @@ app.post('/reg', multer().fields([]), (req, res)=>{
     const pass = req.body.pass;
     const name = req.body.name;
     const lastname = req.body.lastname;
+    // проверить можно ли зарегать пользователя
     connection.execute(
         "INSERT INTO `users`(`login`, `pass`, `name`, `lastname`) VALUES (?,?,?,?)",
         [login, pass, name, lastname]);
     res.json({result: "success"});
 })
-app.get('/auth', multer().fields([]), (req, res)=>{
-    // Выполнить запрос поиска по логину и паролю пользователя
-    // Выведите в консоль найденного пользователя
+app.get('/auth', (req, res)=>{
     res.render('auth', {title: "Авторизация"});
+})
+app.post("/auth", multer().fields([]), (req, res)=>{
+    const login = req.body.login;
+    const pass = req.body.pass;
+    connection.execute("SELECT * FROM users WHERE login=? AND pass=?",
+        [login, pass], (err, resultSet)=>{
+        console.log(resultSet)
+        if(resultSet.length){
+            const user = resultSet[0];
+            const token = bcrypt.hashSync((Date.now()+getIntRandom(10,100)).toString(), saltRounds);
+            console.log(token, user.id)
+            connection.execute("UPDATE users SET token = ? WHERE id = ? ",
+                [token, user.id]);
+            res.cookie("token", token);
+            res.json({result: "success"})
+        }else{
+            res.json({result: "error"})
+        }
+    });
 })
 app.get('/about', (req,  res)=>{
     res.statusCode = 200;
